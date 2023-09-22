e9443a6d284e86cb66ceef7c20fa2fc0b5e921c6
Nguyen Thai Thao Uyen (Chloe) <thaouyen.nttu@gmail.com>
2023-09-22 09:58:36 -0400

    Initial commit Added main screen Added item display card view No function in the MainActivity

---
+++ app/src/main/java/com/example/cs388_project_2_wishlistapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cs388_project_2_wishlistapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
